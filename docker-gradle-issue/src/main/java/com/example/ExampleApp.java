bb4a261bf3fb83f1b321ddf9c9154b31b35e38ae
Vadym S. Khondar <v.khondar@invisilabs.com>
2016-02-23 11:08:30 +0200

    Sample project demonstrating classpath overrides

---
+++ docker-gradle-issue/src/main/java/com/example/ExampleApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by vadya on 02.02.16.
+ */
+package com.example;
+
+public class ExampleApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
